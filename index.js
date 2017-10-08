0a8862b02e867fa5902df23a503e0df7a4fe4c61
Ansel Bridgewater <iamansel@gmail.com>
2017-10-07 23:55:41 -0700

    installed APIs and added hidden config file

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 var express = require('express')
 var exphbs = require('express-handlebars')
+var request = require('request')
+var yelp = require('yelp-fusion')
+var nutritionix = require('nutritionix')
 var app = express()
+
+// APIS
+// nutritionix - nutrient
+// yelp - restaunts
+// edamam - recipes
+//
+// Custom function
+// meal planning "rotation"
+// grocery list generation
+// reasons to be vegan?
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.get('/', function (req, res) {
     res.render('home', { msg: 'Hello World' })
+})
+
+request('https://api.edamam.com/search?q=chicken&app_id=' + edamamConfig.id + '&app_key=' + edamamConfig.key, function (error, response, body) {
+    console.log('error:', error)
+    console.log('statusCode:', response && response.statusCode)
+    console.log('body:', body)
 })
 
 app.listen(3000, function () {
